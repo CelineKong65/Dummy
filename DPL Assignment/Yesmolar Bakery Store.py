64b85297d682369c6cb07d25bac352cb755d3541
LOY YU XUAN <1221208223@student.mmu.edu.my>
2025-05-02 01:30:37 +0800

    adding status to product function

--- DPL Assignment/Yesmolar Bakery Store.py
+++ DPL Assignment/Yesmolar Bakery Store.py
@@ -1,16 +1,14 @@
 import os
 
 # Constants
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))### Bulid in fucntion, remember to remove when add to DPL assignment code
-PRODUCT_FILE = os.path.join(SCRIPT_DIR, "product.txt")
-MEMBERS_FILE = os.path.join(SCRIPT_DIR, "member.txt")
-MEMBERS_ID_FILE = os.path.join(SCRIPT_DIR, "member_id.txt")
-ADMINS_FILE = os.path.join(SCRIPT_DIR, "admin.txt")
-
+MEMBERS_FILE = "member.txt"
+MEMBERS_ID_FILE = "member_id.txt"
+ADMINS_FILE = "admin.txt"
+PRODUCT_FILE = "product.txt"
 
 # Structures
 class Member:
-    def __init__(self, full_name="", member_id="", email="", password="", age="", gender="" , contact="", status="Active"):
+    def __init__(self, full_name="", member_id="", email="", password="", age="", gender="" , contact=""):
         self.member_id = member_id
         self.full_name = full_name
         self.email = email
@@ -18,13 +16,12 @@ class Member:
         self.age = age
         self.gender = gender
         self.contact = contact
-        self.status = status
     
     def __str__(self):
-        return f"{self.member_id}\n{self.full_name}\n{self.email}\n{self.password}\n{self.age}\n{self.gender}\n{self.contact}\n{self.status}"
+        return f"{self.member_id}\n{self.full_name}\n{self.email}\n{self.password}\n{self.age}\n{self.gender}\n{self.contact}"
     
 class Admin:
-    def __init__(self, name="", password="", contact="", position="admin", status="Active"):
+    def __init__(self, name="", password="", contact="", position="admin"):
         allowed_positions = ["admin", "superadmin"]
         if position not in allowed_positions:
             raise ValueError(f"Invalid position: {position}. Must be 'admin' or 'superadmin'.")
@@ -33,27 +30,28 @@ class Admin:
         self.password = password
         self.contact = contact
         self.position = position
-        self.status = status
 
     def __str__(self):
-        return f"{self.name}\n{self.password}\n{self.contact}\n{self.position}\n{self.status}"
+        return f"{self.name}\n{self.password}\n{self.contact}\n{self.position}"
 
 class Product:
-    def __init__(self, product_id="", name="", category="", price=0.0, stock=0):
+    def __init__(self, product_id="", name="", category="", price=0.0, stock=0, status=""):
         self.product_id = product_id
         self.name = name
         self.category = category
         self.price = price
         self.stock = stock
+        self.status = status
 
 class CartItem:
-    def __init__(self, product=None, product_id="", name="", price=0.0, quantity=0, total=0.0):
+    def __init__(self, product=None, product_id="", name="", price=0.0, quantity=0, total=0.0, status=""):
         self.product = product if product else Product()
         self.product_id = product_id if product_id else (product.product_id if product else "")
         self.name = name if name else (product.name if product else "")
         self.price = price if price else (product.price if product else 0.0)
         self.quantity = quantity
         self.total = total if total else (self.price * quantity)
+        self.status = status if status else (product.status if product else "")
 
 # Global variables
 products = []
@@ -75,7 +73,7 @@ def load_members():
                 if line != "":
                     data_lines.append(line)
 
-                if len(data_lines) == 8:
+                if len(data_lines) == 7:
                     member = Member(
                         member_id=data_lines[0],
                         full_name=data_lines[1],
@@ -83,8 +81,7 @@ def load_members():
                         password=data_lines[3],
                         age=data_lines[4],
                         gender=data_lines[5],
-                        contact=data_lines[6],
-                        status=data_lines[7]
+                        contact=data_lines[6]
                     )
                     members.append(member)
                     data_lines = []
@@ -106,16 +103,15 @@ def get_next_member_id():
                 return "U0001"
     except FileNotFoundError:
         with open(MEMBERS_ID_FILE, "w", encoding='utf-8') as file:
-            file.write("\nU0001\n")
+            file.write("U0001\n")
         return "U0001"
 
 def save_member(member):
     with open(MEMBERS_FILE, "a", encoding='utf-8') as file:
-        file.write(f"\n\n{member.member_id}\n{member.full_name}\n{member.email}\n{member.password}\n{member.age}\n{member.gender}\n{member.contact}\n{member.status}\n\n")
+        file.write(f"\n\n{member.member_id}\n{member.full_name}\n{member.email}\n{member.password}\n{member.age}\n{member.gender}\n{member.contact}\n\n")
 
 def signup():
     global logged_in_member 
-    status = "Active"
 
     # Member id
     member_id = get_next_member_id()
@@ -292,7 +288,7 @@ def signup():
     elif password != confirm_password:
         print("Error : Passwords do not match!")
     else:
-        new_member = Member(full_name=full_name, member_id=member_id, email=email, password=password, age=age, gender=gender, contact=contact,status=status)
+        new_member = Member(full_name=full_name, member_id=member_id, email=email, password=password, age=age, gender=gender, contact=contact)
         members.append(new_member)
 
         with open(MEMBERS_ID_FILE, "a", encoding='utf-8') as file:
@@ -302,15 +298,6 @@ def signup():
         print(f"Registration successful! Your Member ID: {member_id}")
         input("\nPress [ENTER] to return to login menu.")
         clear_screen()
-
-def to_lower_case(s):
-    result = ""
-    for char in s:
-        if 'A' <= char <= 'Z':
-            result += chr(ord(char) + 32)
-        else:
-            result += char
-    return result
 
 def login():
     global logged_in_member 
@@ -322,18 +309,11 @@ def login():
         with open(MEMBERS_FILE, "r", encoding='utf-8') as f:
             lines = [line.strip() for line in f if line.strip() != ''] 
 
-        for i in range(0, len(lines), 8):  
+        for i in range(0, len(lines), 7):  
             stored_email = lines[i + 2]
             stored_password = lines[i + 3]
-            status = lines[i + 7]
 
             if email == stored_email:
-                if to_lower_case(status) != "active":
-                    print("Your account is inactive. Please contact admin.")
-                    input("\nPress [ENTER] to return to login menu.")
-                    clear_screen()
-                    return False
-                
                 attempts = 0
                 while attempts < 3:
                     if password == stored_password:
@@ -345,8 +325,7 @@ def login():
                             password=lines[i + 3],
                             age=lines[i + 4],
                             gender=lines[i + 5],
-                            contact=lines[i + 6],
-                            status=lines[i + 7]
+                            contact=lines[i + 6]
                         )
                         input("\nPress [ENTER] to continue.")
                         return main_menu()
@@ -389,8 +368,7 @@ def update_member(updated_member):
                     updated_member.password,
                     str(updated_member.age),
                     updated_member.gender,
-                    updated_member.contact,
-
+                    updated_member.contact
                 ]
                 updated_members.append("\n".join(new_member_data))
             else:
@@ -410,6 +388,8 @@ def member_profile():
     global logged_in_member 
 
     clear_screen()
+
+    
 
     while True:
         print("------------------------------------------------------------------")
@@ -456,15 +436,17 @@ def edit_member_profile():
 
         choice = input("\nSelect the number you want to edit (1-8): ")
 
+        # Member ID
         if choice == "1":
             input("\nMember ID cannot be edited. Press [ENTER] to continue.")
-
+        # Full Name
         elif choice == "2":
             logged_in_member.full_name = input("Enter new Full Name: ")
             update_member(logged_in_member)
             print("Full Name updated successfully!")
             input("Press [ENTER] to continue.")
 
+        # Email
         elif choice == "3":
             while True:
                 logged_in_member.email = input("Enter your new email (example: xuanting@example.com): ")
@@ -491,6 +473,7 @@ def edit_member_profile():
 
                 break
 
+        # Password
         elif choice == "4":
             while True:
                 logged_in_member.password = ""
@@ -532,6 +515,7 @@ def edit_member_profile():
                 input("Press [ENTER] to continue.")
                 break
 
+        # Age
         elif choice == "5":
             try:
                 while True:
@@ -561,6 +545,7 @@ def edit_member_profile():
                 print("Invalid input! Age must be a number.")
             input("Press [ENTER] to continue.")
 
+        # Gender
         elif choice == "6":
             while True:
                 logged_in_member.gender = input("Enter new gender (male or female): ")
@@ -592,6 +577,7 @@ def edit_member_profile():
                 input("Press [ENTER] to continue.")
                 break
 
+        # Contact Number
         elif choice == "7":
             while True:
                 logged_in_member.contact  = input("Enter your contact number (example: 012-34567890): ")
@@ -632,6 +618,7 @@ def edit_member_profile():
                 input("Press [ENTER] to continue.")
                 break
         
+        # Return to view profile
         elif choice == "8":
             input("\nPress [ENTER] to return to your profile.")
             clear_screen()
@@ -650,30 +637,22 @@ def admin_login():
         with open(ADMINS_FILE, "r", encoding='utf-8') as f:
             lines = [line.strip() for line in f if line.strip() != ''] 
 
-        for i in range(0, len(lines), 5):  
+        for i in range(0, len(lines), 4):  
             stored_name = lines[i]
             stored_password = lines[i + 1]
             stored_position = lines[i + 3]
-            status = lines[i + 4]
 
             if name == stored_name:
-                if to_lower_case(status) != "active":
-                    print("Your account is inactive. Please contact superadmin.")
-                    input("\nPress [ENTER] to return to login menu.")
-                    clear_screen()
-                    return False
-                
                 attempts = 0
                 while attempts < 3:
                     if password == stored_password:
                         print("Logged in Successfully!")
-                        print(f"Welcome {stored_position}!\n")
+                        print(f"Welcome {stored_position}!\n") # check is admin or superadmin
                         logged_in_admin = Admin(
                             name=lines[i],
                             password=lines[i + 1],
                             contact=lines[i + 2],
-                            position=lines[i + 3],
-                            status=lines[i + 4]
+                            position=lines[i + 3]
                         )
                         input("\nPress [ENTER] to continue.")
                         clear_screen()
@@ -688,11 +667,11 @@ def admin_login():
                 input("\nPress [ENTER] to return to login menu.")
                 clear_screen()
                 return False
-
-            print("Name not found.\n")
-            input("\nPress [ENTER] to continue.")
-            clear_screen()
-            return False
+                    
+        print("Name not found.\n")
+        input("\nPress [ENTER] to continue.")
+        clear_screen()
+        return False
 
     except FileNotFoundError:
         print("Error: Admins file not found!")
@@ -763,12 +742,14 @@ def edit_admin_profile():
 
         choice = input("\nSelect the number you want to edit (1-5): ")
 
+        # Full Name
         if choice == "1":
             logged_in_admin.name = input("Enter new Full Name: ")
             update_admin(logged_in_admin)
             print("Full Name updated successfully!")
             input("Press [ENTER] to continue.")
 
+        # Password
         elif choice == "2":
             while True:
                 logged_in_admin.password = ""
@@ -810,6 +791,7 @@ def edit_admin_profile():
                 input("Press [ENTER] to continue.")
                 break
 
+        # Contact Number
         elif choice == "3":
             while True:
                 logged_in_admin.contact  = input("Enter your contact number (example: 012-34567890): ")
@@ -852,6 +834,7 @@ def edit_admin_profile():
         elif choice == "4":
             input("\nYour position cannot be edited. Press [ENTER] to continue.")
         
+        # Return to view profile
         elif choice == "5":
             input("\nPress [ENTER] to return to your profile.")
             clear_screen()
@@ -954,12 +937,13 @@ def get_quoted_field(ss):
     return field.strip(), ss.strip()
 
 def display_product(product):
-    print("---------------------------------------------------------------")
-    print(f"Product ID: {product.product_id}")
-    print("---------------------------------------------------------------")
-    print(f"| Name     : {product.name}")
-    print(f"| Category : {product.category}")
-    print(f"| Price    : RM {product.price:.2f}")
+    if product.status=="Active":
+        print("---------------------------------------------------------------")
+        print(f"Product ID: {product.product_id}")
+        print("---------------------------------------------------------------")
+        print(f"| Name     : {product.name}")
+        print(f"| Category : {product.category}")
+        print(f"| Price    : RM {product.price:.2f}")
 
     if product.stock <= 0:
         print("| WARNING  : SORRY! THIS PRODUCT IS CURRENTLY OUT OF STOCK!")
@@ -985,6 +969,7 @@ def load_products():
                 product.category, ss = get_quoted_field(ss)
                 price_str, ss = get_quoted_field(ss)
                 stock_str, ss = get_quoted_field(ss)
+                product.status, ss = get_quoted_field(ss)
 
                 try:
                     product.price = float(price_str) if price_str else 0.0
@@ -1017,8 +1002,9 @@ def update_product_file():
                 category = f'"{product.category}"' if ',' in product.category else product.category
                 price = str(product.price)
                 stock = str(product.stock)
+                status = f'"{product.status}"' if ',' in product.status else product.status
 
-                file.write(f"{pid},{name},{category},{price},{stock}\n")
+                file.write(f"{pid},{name},{category},{price},{stock},{status}\n")
         return True
     except IOError as e:
         print(f"Error: Could not update the product file: {e}")
@@ -1053,6 +1039,7 @@ def load_cart(cart):
                 item = CartItem()
                 item.product_id = parts[1].strip()
                 item.name = parts[2].strip()
+                item.status = parts[3].strip()
                 
                 try:
                     item.price = float(parts[3].strip())
@@ -1086,6 +1073,7 @@ def save_cart(cart):
                 price = item.price if item.price is not None else (item.product.price if item.product else 0.0)
                 quantity = item.quantity
                 total = item.total if item.total is not None else (price * quantity)
+                status = item.status if item.status else (item.product.status if item.product else "")
 
                 file.write(f"{logged_in_member},{pid},{name},{price:.2f},{quantity},{total:.2f}\n")
         return True
@@ -1140,6 +1128,7 @@ def display_cart(cart):
         price = item.price if item.price is not None else (item.product.price if item.product else 0.0)
         quantity = item.quantity
         total = item.total if item.total is not None else (price * quantity)
+        status = item.product.status if item.product and item.product.status else "N/A"
 
         print(f"Item {i}:")
         print(f"Product ID : {pid}")
@@ -1149,6 +1138,10 @@ def display_cart(cart):
         print(f"Quantity   : {quantity}")
         print(f"Total      : RM {total:.2f}")
         print("------------------------------------------------------------------")
+
+        if status=="Inative":
+            print("SORRY! This product is currently unavailable :(")
+            total=0.0
 
         grand_total += total
 
@@ -1236,7 +1229,8 @@ def add_to_cart(cart, product_id, quantity):
             product_id=selected_product.product_id,
             name=selected_product.name,
             price=selected_product.price,
-            quantity=quantity
+            quantity=quantity,
+            status=selected_product.status
         ))
 
     selected_product.stock -= quantity
